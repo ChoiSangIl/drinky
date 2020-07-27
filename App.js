9f6f2da48d87e0e28016ad09f973f4691d280068
ChoiSangIl <sang12@kakao.com>
2020-07-27 21:45:34 +0900

    하단 icon 적용

--- App.js
+++ App.js
@@ -5,23 +5,37 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './screens/HomeScreen.js';
 import CalendarScreen from './screens/CalendarScreen.js';
 import StatisticsScreen from './screens/StatisticsScreen.js';
-
-
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
-
+import SettingsScreen from './screens/SettingScreen.js';
+import Icon from 'react-native-vector-icons/Ionicons';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === '메인') {
+              iconName = focused ? 'beer' : 'beer-outline';
+            } else if (route.name === '달력') {
+              iconName = focused ? 'calendar' : 'calendar-outline';
+            } else if (route.name === '통계'){
+              iconName = focused ? 'podium' : 'podium-outline';
+            } else if (route.name === '설정'){
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Icon name={iconName} size={size}  color={color}/>;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'black',
+          inactiveTintColor: 'gray',
+        }}
+      >
         <Tab.Screen name="메인" component={HomeScreen} />
         <Tab.Screen name="달력" component={CalendarScreen} />
         <Tab.Screen name="통계" component={StatisticsScreen} />
