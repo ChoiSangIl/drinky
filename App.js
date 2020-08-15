aaffba0f6051145b07208969f1942eac3008ab99
ChoiSangIl <sang12@kakao.com>
2020-08-15 16:40:08 +0900

    소주 이미지 추가

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import HomeScreen from './screens/HomeScreen.js';
 import CalendarScreen from './screens/CalendarScreen.js';
 import StatisticsScreen from './screens/StatisticsScreen.js';
 import SettingsScreen from './screens/SettingScreen.js';
 import Icon from 'react-native-vector-icons/Ionicons';
-const Tab = createBottomTabNavigator();
+
+const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
+          tabBarIcon: ({ focused, color }) => {
             let iconName;
-
             if (route.name === '메인') {
               iconName = focused ? 'beer' : 'beer-outline';
             } else if (route.name === '달력') {
@@ -26,15 +26,18 @@ export default function App() {
             } else if (route.name === '설정'){
               iconName = focused ? 'settings' : 'settings-outline';
             }
-
             // You can return any component that you like here!
-            return <Icon name={iconName} size={size}  color={color}/>;
+            return <Icon name={iconName} size={20}  color={color}/>;
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'black',
-          inactiveTintColor: 'gray',
+          labelStyle: { fontSize: 12 },
+          tabStyle: { width: 100 },
+          style: { backgroundColor: 'white' },
+          showIcon: true,
+          showLabel: false
         }}
+        tabBarPosition="bottom"
       >
         <Tab.Screen name="메인" component={HomeScreen} />
         <Tab.Screen name="달력" component={CalendarScreen} />
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import Draggable from 'react-native-draggable';
 
+const styles = StyleSheet.create({
+  soju: {
+    width: 70,
+    height: 130,
+  }
+});
 const HomeScreen = () => {
     return (
       <View>
-        <Text>HomeScreen</Text>      
+        <Draggable x={200} y={300} renderColor='red' renderText='B'/>
+        <Draggable x={50} y={50}  renderSize={100} shouldReverse onShortPressRelease={()=>alert('touched!!')}>
+          <Image style={styles.soju} source={require('../image/soju.png')} />
+        </Draggable>
       </View>
     );
 };
